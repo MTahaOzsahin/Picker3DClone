ac597e05f92ee00e047c8c8e98e0171dfe45d505
MTahaOzsahin <87945619+MTahaOzsahin@users.noreply.github.com>
2023-10-27 19:29:46 +0300

    Setting ups fundumentals and imported some assets.

---
+++ Assets/Scripts/Controller/PlayerController/PlayerMovementController.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Controller.PlayerController
+{
+    public class PlayerMovementController : MonoBehaviour
+    {
+        [Header("Player Movement Speed"), SerializeField]
+        private float playerMovementSpeed;
+        
+        
+        
+    }
+}
